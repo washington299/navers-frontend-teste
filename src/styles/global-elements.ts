c22272126176ebe623049afa5304d758d3680db9
Washington Campos <washingtoncampos9@gmail.com>
2020-08-20 02:44:51 -0300

    feat: create Logo and Button elements

---
+++ src/styles/global-elements.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Logo = styled.img.attrs({
+  src: 'assets/images/logo-and-name.png',
+  alt: 'Navers Logo',
+})`
+`;
+
+export const Button = styled.button`
+  height: 40px;
+  font-weight: 600px;
+  font-size: var(--normal-font-size);
+  border: 1px solid var(--primary-color);
+`;
