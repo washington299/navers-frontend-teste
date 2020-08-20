0430eb6b2241a4b6c61767f37bc37e22731d99f3
Washington Campos <washingtoncampos9@gmail.com>
2020-08-20 00:27:12 -0300

    chore: set up react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
 
 import GlobalStyles from './styles/global-styles';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <GlobalStyles />
-      <h1>Hello Nave</h1>
-    </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
