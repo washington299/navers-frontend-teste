57aa93526247450b3f0d633c4383c60f469a4e45
Washington Campos <washingtoncampos9@gmail.com>
2020-08-20 16:11:11 -0300

    refactor: create FieldArea, Label and Input global elements

--- src/styles/global-elements.ts
+++ src/styles/global-elements.ts
@@ -6,6 +6,35 @@ export const Logo = styled.img.attrs({
 })`
 `;
 
+export const FieldArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 40px;
+
+  span {
+    margin-top: 5px;
+    color: red;
+    font-size: var(--small-font-size);
+    font-weight: 600;
+    text-align: start;
+  }
+`;
+
+export const Label = styled.label`
+  color: var(--primary-color);
+  font-size: var(--normal-font-size);
+  font-weight: 600;
+  text-align: start;
+  margin-bottom: 5px;
+`;
+
+export const Input = styled.input`
+  height: 40px;
+  padding: 0 10px;
+  font-size: var(--normal-font-size);
+  border: 1px solid #424242;
+`;
+
 export const Button = styled.input`
   height: 40px;
   font-weight: 600px;
