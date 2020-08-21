625e87fa60166f6e02276ab4ac453efc93369712
Washington Campos <washingtoncampos9@gmail.com>
2020-08-21 05:06:01 -0300

    feat: redirect user to main page if valid credentials are provided

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,22 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
-import { Input, Label } from '../../styles/global-elements';
+import api from '../../services/api';
+import { useCredentialsDispatch } from '../../contexts/credentials';
+
 import {
-  Container, Card, FormLogo, FormFieldArea, FormButton,
+  Container,
+  Card,
+  FormLogo,
+  FormFieldArea,
+  FormButton,
 } from './styles';
+import { Input, Label } from '../../styles/global-elements';
 
 const Login: React.FC = () => {
+  const history = useHistory();
+  const [invalidCredentials, setInvalidCredentials] = useState(false);
+  const dispatch = useCredentialsDispatch();
   const { handleSubmit, register, errors } = useForm();
 
-  function onSubmit(values: any) {
-    console.log(values);
+  async function onSubmit(values: any) {
+    const { email, password } = values;
+    api.post('/users/login', { email, password })
+      .then((res) => {
+        dispatch({
+          type: 'GET_USER_CREDENTIALS',
+          payload: {
+            id: res.data.id,
+            email: res.data.email,
+            token: res.data.token,
+          },
+        });
+        history.push('/navers');
+      })
+      .catch(() => setInvalidCredentials(true));
   }
 
   return (
     <Container>
       <Card onSubmit={handleSubmit(onSubmit)}>
         <FormLogo height={50} />
+        {invalidCredentials && <span>E-mail e/ou senha incorretos!!!</span>}
         <FormFieldArea>
           <Label htmlFor="email">E-mail</Label>
           <Input
@@ -33,7 +58,7 @@ const Login: React.FC = () => {
             type="password"
             name="password"
             placeholder="Senha"
-            ref={register({ required: 'Campo obrigatório!!!' })}
+            ref={register({ required: 'Campo obrigatório!!!', minLength: 6 })}
           />
           {errors.password && <span>{errors.password.message}</span>}
         </FormFieldArea>
