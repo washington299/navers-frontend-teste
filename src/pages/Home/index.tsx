9e8477bf7f136dcf95b0a52e86db2c4828b67383
Washington Campos <washingtoncampos9@gmail.com>
2020-08-22 20:43:34 -0300

    feat: get all navers data

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
+import { INaver } from '../../interfaces';
+import { useCredentialsState } from '../../contexts/credentials';
 
 import Header from '../../components/Header';
+import Card from '../../components/Card';
 
 import {
   HomeStyles,
   Top,
   Title,
   AddButton,
+  Main,
 } from './styles';
 
-const Home: React.FC = () => (
-  <>
-    <Header />
-    <HomeStyles>
-      <Top>
-        <Title>Navers</Title>
-        <AddButton defaultValue="Adicionar Naver" />
-      </Top>
-    </HomeStyles>
-  </>
-);
+const Home: React.FC = () => {
+  const [navers, setNavers] = useState<INaver[]>([]);
+  const history = useHistory();
+  const { token } = useCredentialsState();
+
+  useEffect(() => {
+    function getNavers() {
+      api.get('/navers?', { headers: { Authorization: `Bearer ${token}` } })
+        .then((res) => setNavers(res.data))
+        .catch(() => {});
+    }
+    getNavers();
+  });
+
+  return (
+    <>
+      <Header />
+      <HomeStyles>
+        <Top>
+          <Title>Navers</Title>
+          <AddButton defaultValue="Adicionar Naver" />
+        </Top>
+        <Main>
+          {navers.map((naver) => (
+            <Card naver={naver} />
+          ))}
+        </Main>
+      </HomeStyles>
+    </>
+  );
+};
 
 export default Home;
