302dc17165d96a71d1c2f814665d5e07cbbad9ba
Washington Campos <washingtoncampos9@gmail.com>
2020-08-21 22:47:00 -0300

    feat: add private routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import { CredentialsProvider } from './contexts/credentials';
+import PrivateRouter from './routers/private-router';
+
 import Login from './pages/Login';
+import Home from './pages/Home';
 
 import GlobalStyles from './styles/global-styles';
 
 function App() {
   return (
-    <>
+    <CredentialsProvider>
       <GlobalStyles />
       <Router>
         <Switch>
+          <PrivateRouter path="/navers">
+            <Home />
+          </PrivateRouter>
           <Route exact path="/" component={Login} />
         </Switch>
       </Router>
-    </>
+    </CredentialsProvider>
   );
 }
 
