2f166a461da4bfff9e60154d1552fd5cf23e4259
Washington Campos <washingtoncampos9@gmail.com>
2020-08-24 04:27:02 -0300

    feat: display naver data modal on click

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Edit, Delete, Close } from '@material-ui/icons';
+
 import { INaver } from '../../interfaces';
 
 import {
-  Container, Name, Job, Icons,
+  Container,
+  Name,
+  Job,
+  Icons,
+  InformationArea,
+  Title,
+  SubTitle,
+  Text,
+  NaverData,
+  DeleteNaver,
 } from './styles';
+import { DarkBackground } from '../../styles/global-elements';
 
 type Props = {
   naver: INaver,
 };
 
-const Card: React.FC<Props> = ({ naver }: Props) => (
-  <Container>
-    <img src={naver.url} alt={`${naver.name} - ${naver.job_role}`} />
-    <Name>{naver.name}</Name>
-    <Job>{naver.job_role}</Job>
-    <Icons>
-      <div>edit</div>
-      <div>delete</div>
-    </Icons>
-  </Container>
-);
+const Card: React.FC<Props> = ({ naver }: Props) => {
+  const [displayNaverDataBox, setDisplayNaverDataBox] = useState(false);
+  const [displayDeleteNaverBox, setDisplayDeleteNaverBox] = useState(false);
+
+  function handleDeleteNaver() {
+    setDisplayDeleteNaverBox(true);
+  }
+
+  return (
+    <>
+      {displayNaverDataBox && (
+        <DarkBackground>
+          <NaverData>
+            <img src={naver.url} alt={`${naver.name} - ${naver.job_role}`} />
+            <InformationArea>
+              <span className="close-button">
+                <Close onClick={() => setDisplayNaverDataBox(false)} />
+              </span>
+              <Title>{naver.name}</Title>
+              <Text>{naver.job_role}</Text>
+              <SubTitle>Idade</SubTitle>
+              <Text>{naver.birthdate}</Text>
+              <SubTitle>Tempo de empresa</SubTitle>
+              <Text>{naver.admission_date}</Text>
+              <SubTitle>Projetos que ja participou</SubTitle>
+              <Text>{naver.project}</Text>
+            </InformationArea>
+          </NaverData>
+        </DarkBackground>
+      )}
+      {displayDeleteNaverBox && (
+        <DarkBackground>
+          <DeleteNaver>Deletar Naver</DeleteNaver>
+        </DarkBackground>
+      )}
+      <Container onClick={() => setDisplayNaverDataBox(true)}>
+        <img src={naver.url} alt={`${naver.name} - ${naver.job_role}`} />
+        <Name>{naver.name}</Name>
+        <Job>{naver.job_role}</Job>
+        <Icons>
+          <Edit />
+          <Delete onClick={handleDeleteNaver} />
+        </Icons>
+      </Container>
+    </>
+  );
+};
 
 export default Card;
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -5,16 +5,101 @@ export const Container = styled.div`
     width: 100%;
     height: 280px;
   }
+
+  :hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
 `;
 
 export const Name = styled.h1`
-
+  margin: 10px 0;
+  font-size: var(--normal-font-size);
+  font-weight: 600;
 `;
 
 export const Job = styled.span`
-
+  display: block;
+  margin: 10px 0;
+  font-size: var(--normal-font-size);
 `;
 
 export const Icons = styled.div`
+  display: flex;
+  color: var(primary--color);
 
+  svg {
+    margin-right: 10px;
+    cursor: pointer;
+  }
+`;
+
+export const NaverData = styled.div`
+  width: 80%;
+  height: auto;
+  background-color: var(--secondary-color);
+  display: flex;
+  flex-direction: column-reverse;
+
+  img {
+    width: 100%;
+    flex: 1;
+  }
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+export const InformationArea = styled.div`
+  flex: 1;
+
+  .close-button {
+    display: block;
+    text-align: end;
+    cursor: pointer;
+  }
+`;
+
+export const Title = styled.h1`
+  margin: 0 15px;
+  font-size: var(--big-font-size);
+
+  @media screen and (min-width: 960px) {
+    font-size: var(--extra-big-font-size);
+    margin: 0 20px;
+  }
+`;
+
+export const SubTitle = styled(Title)`
+  font-size: 16px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+  }
+
+  @media screen and (min-width: 960px) {
+    font-size: var(--big-font-size);
+  }
+`;
+
+export const Text = styled.span`
+  display: block;
+  margin: 15px 15px;
+  font-size: 14px;
+
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+    margin: 15px 20px;
+  }
+  @media screen and (min-width: 768px) {
+    font-size: 22px;
+    margin: 25px 20px;
+  }
+`;
+
+export const DeleteNaver = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: var(--secondary-color);
 `;
