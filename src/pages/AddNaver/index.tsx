57e65f1840d8a7aa642f445bbef31ead266161ad
Washington Campos <washingtoncampos9@gmail.com>
2020-08-29 01:23:06 -0300

    feat: create inputs error messages

--- src/pages/AddNaver/index.tsx
+++ src/pages/AddNaver/index.tsx
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { ArrowBackIos } from '@material-ui/icons';
 
+import api from '../../services/api';
+
 import Header from '../../components/Header';
 
 import {
   Container, Content, Top, Title, Form, FormFieldArea, FormButton,
 } from './styles';
-import { Label, Input } from '../../styles/global-elements';
+import { Label, Input, AlertMsg } from '../../styles/global-elements';
 
 const AddNaver: React.FC = () => {
   const { register, handleSubmit, errors } = useForm();
@@ -31,27 +33,33 @@ const AddNaver: React.FC = () => {
           <Form onSubmit={handleSubmit(onSubmit)}>
             <FormFieldArea>
               <Label>Nome</Label>
-              <Input type="text" name="name" placeholder="Nome" ref={register} />
+              <Input type="text" name="name" placeholder="Nome" ref={register({ required: 'Campo obrigatório!!!' })} />
+              {errors.name && <AlertMsg>{errors.name.message}</AlertMsg>}
             </FormFieldArea>
             <FormFieldArea>
               <Label>Idade</Label>
-              <Input type="date" name="birthdate" placeholder="Idade" ref={register} />
+              <Input type="date" name="birthdate" placeholder="Idade" ref={register({ required: 'Campo obrigatório!!!' })} />
+              {errors.birthdate && <AlertMsg>{errors.birthdate.message}</AlertMsg>}
             </FormFieldArea>
             <FormFieldArea>
               <Label>Projetos que participou</Label>
-              <Input type="text" name="project" placeholder="Projetos que participou" ref={register} />
+              <Input type="text" name="project" placeholder="Projetos que participou" ref={register({ required: 'Campo obrigatório!!!' })} />
+              {errors.project && <AlertMsg>{errors.project.message}</AlertMsg>}
             </FormFieldArea>
             <FormFieldArea>
               <Label>cargo</Label>
-              <Input type="text" name="job_role" placeholder="Cargo" ref={register} />
+              <Input type="text" name="job_role" placeholder="Cargo" ref={register({ required: 'Campo obrigatório!!!' })} />
+              {errors.job_role && <AlertMsg>{errors.job_role.message}</AlertMsg>}
             </FormFieldArea>
             <FormFieldArea>
               <Label>Tempo de empresa</Label>
-              <Input type="date" name="admission_date" placeholder="Tempo de empresa" ref={register} />
+              <Input type="date" name="admission_date" placeholder="Tempo de empresa" ref={register({ required: 'Campo obrigatório!!!' })} />
+              {errors.admission_date && <AlertMsg>{errors.admission_date.message}</AlertMsg>}
             </FormFieldArea>
             <FormFieldArea>
               <Label>URL da foto do naver</Label>
-              <Input type="text" name="url" placeholder="URL da foto do naver" ref={register} />
+              <Input type="text" name="url" placeholder="URL da foto do naver" ref={register({ required: 'Campo obrigatório!!!' })} />
+              {errors.url && <AlertMsg>{errors.url.message}</AlertMsg>}
             </FormFieldArea>
             <FormButton type="submit">Salvar</FormButton>
           </Form>
