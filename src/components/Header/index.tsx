6f888c4be4360f24e193bccc7bdfcfd5cf164aaf
Washington <washingtoncampos9@gmail.com>
2020-09-06 03:11:40 -0300

    feat: create logout and clear cookie actions

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 
-import { useCredentialsDispatch } from '../../contexts/credentials';
+import { Log } from '../../services/auth';
 
 import { HeaderStyle, Logo, SignOut } from './styles';
 
 const Header: React.FC = () => {
   const history = useHistory();
-  const dispatch = useCredentialsDispatch();
 
   function handleClick() {
-    dispatch({
-      type: 'REMOVE_USER_CREDENTIALS',
-      payload: { id: '', email: '', token: '' },
-    });
-
-    history.push('/');
+    Log.doLogOut();
+    window.location.href = '/';
   }
 
   return (
