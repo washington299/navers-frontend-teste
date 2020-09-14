f0b6431dd33d8c3470a8a63737821a8c9a54f37f
Washington <washingtoncampos9@gmail.com>
2020-09-14 17:39:51 -0300

    refactor: delete credentials context file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { CredentialsProvider } from './contexts/credentials';
 import { NaverProvider } from './contexts/naver';
 import PrivateRouter from './routers/private-router';
 
@@ -14,25 +13,23 @@ import GlobalStyles from './styles/global-styles';
 
 function App() {
   return (
-    <CredentialsProvider>
-      <NaverProvider>
-        <GlobalStyles />
-        <Router>
-          <Switch>
-            <Route exact path="/sign-in" component={Login} />
-            <PrivateRouter path="/add">
-              <AddNaver />
-            </PrivateRouter>
-            <PrivateRouter path="/edit/:id">
-              <EditNaver />
-            </PrivateRouter>
-            <PrivateRouter path="/">
-              <Home />
-            </PrivateRouter>
-          </Switch>
-        </Router>
-      </NaverProvider>
-    </CredentialsProvider>
+    <NaverProvider>
+      <GlobalStyles />
+      <Router>
+        <Switch>
+          <Route exact path="/sign-in" component={Login} />
+          <PrivateRouter path="/add">
+            <AddNaver />
+          </PrivateRouter>
+          <PrivateRouter path="/edit/:id">
+            <EditNaver />
+          </PrivateRouter>
+          <PrivateRouter path="/">
+            <Home />
+          </PrivateRouter>
+        </Switch>
+      </Router>
+    </NaverProvider>
   );
 }
 
